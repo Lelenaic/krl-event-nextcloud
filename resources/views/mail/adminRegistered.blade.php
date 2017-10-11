f1c4ba87eaaad7d58bab30c5414627bf4aa7d36d
Lénaïc Grolleau <lenaic@lenaic.me>
2017-10-11 16:18:56 +0200

    Added reduced to mail

--- resources/views/mail/adminRegistered.blade.php
+++ resources/views/mail/adminRegistered.blade.php
@@ -2,6 +2,7 @@ Nouvelle inscription au Workshop Docker :<br>
 Nom : {{$ticket->name}}<br>
 Mail : {{$ticket->email}}<br>
 Moyen de paiement : {{$ticket->card?'Carte':'Chèque'}}<br>
+Tarif réduit : {{$ticket->reduced ? 'Oui':'Non'}}<br>
 @if($ticket->card)
     Numéro de transaction : {{$number}}
 @endif
